518579f7fb49661794f23b062452d77ccef9caf3
Adam Welnicki <adam.welnicki@gmail.com>
2021-10-17 11:14:38 +0200

    Commit 2 (zad1 i zad2)

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-print(20)
+# ---------------------------------------------------------------------
+# Zadanie 1
+# ---------------------------------------------------------------------
+# firstname = input("Podaj pierwszą literę imienia: ")
+# surname = input("Podaj nazwisko: ")
+#
+#
+# def inicjaly(a, b):
+#     return print(a + "." + b)
+#
+#
+# inicjaly(firstname, surname)
+# ---------------------------------------------------------------------
+# Zadanie 2
+# ---------------------------------------------------------------------
+# firstname = input("Podaj imię: ")
+# surname = input("Podaj nazwisko: ")
+#
+#
+# def inicjaly(a, b):
+#     return print(a[0].capitalize() + "." + b.capitalize())
+#
+#
+# inicjaly(firstname, surname)
+
 
